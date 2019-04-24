63af4aa2bfdb1ee49b81edd94f4260993b20f859
1-8192 <allegranzia@gmail.com>
2019-04-23 22:12:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleClick = () => {
+  return alert('I have been clicked!')
+}
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleClick} />
   </div>,
   document.getElementById('root')
 );
